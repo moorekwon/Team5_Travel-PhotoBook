183776ab60bb79fed0b78d00084b5fe10f37adff
suellaiy <kwarusai@naver.com>
2020-01-17 17:36:32 +0900

    edit page sample구현 및 원하는 image갯수를 지정

--- app/photobook/urls.py
+++ app/photobook/urls.py
@@ -4,5 +4,6 @@ from photobook import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.album_create, name='album_create'),
-    path('page_1/<int:photobook_pk>', views.page_create, name='page_1'),
+    path('page_1/<int:photobook_pk>/', views.page_create, name='page_1'),
+    path('page_1/edit/<int:page_pk>/', views.edit_page, name='edit_page')
 ]
